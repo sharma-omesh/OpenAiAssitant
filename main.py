f29d1a47635b218eb735ee49b6879610fbacf1bb
rajahaseebfayyaz <rajahaseebfayyaz@gmail.com>
2024-02-14 15:07:14 +0530

    added main

--- main.py
+++ main.py
@@ -158,4 +158,4 @@ async def post_thread(thread_id: str, message: CreateMessage):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
